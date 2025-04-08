7e569c090bb0f366cdb64e168c52ec231c55777a
Arthur Xavier <arthur19xavier@gmail.com>
2025-04-08 01:16:21 -0300

    Adição de nova function Frear

--- index.ts
+++ index.ts
@@ -23,6 +23,9 @@ while(true){
         case 1:
             acelerar(carro);
             break;
+        case 2:
+            frear(carro);
+            break;
         case 5:
             console.table(carro);
             break; 
@@ -38,6 +41,17 @@ function acelerar(veiculo: Veiculo): void{
     veiculo.velocidade += veiculo.potencia*0.1;
     console.log(veiculo.velocidade);
 }}
+
+function frear(veiculo: Veiculo): void {
+    if (veiculo.marchaAtual != 0 && veiculo.velocidade > 0) {
+        veiculo.velocidade -= veiculo.potencia * 0.1;
+        if (veiculo.velocidade < 0) {
+            veiculo.velocidade = 0; 
+        }
+        console.log(veiculo.velocidade);
+    }
+}
+
 
 function criaVeiculo(): Veiculo{
     const veiculo: Veiculo = new Veiculo();
